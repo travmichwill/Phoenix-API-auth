ef7e093f407363c24e75cbed4900f8fc2b721771
rpkittle <travismw28@yahoo.com>
2016-06-09 08:56:05 -0500

    Added Login Func

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -8,7 +8,15 @@ defmodule AuthApi.Auth do
   
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
+	#user_id = fetch_session(conn, :user_id)
 	user = user_id && repo.get(AuthApi.User, user_id)
 	assign(conn, :current_user, user)
+  end
+  
+  def login(conn, user) do
+    conn
+	|> assign(:current_user, user)
+	|> put_session(:user_id, user.id)
+	|> configure_session(renew: true)
   end
 end
